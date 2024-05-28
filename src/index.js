5ad1ae017bffdb3a426a5a79f55eb17ab7a1775d
luke <luke_tu@brown.edu>
2024-05-28 18:30:45 -0500

    all

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-const PUBLIC_URL = process.env.PUBLIC_URL; // This will automatically refer to your homepage value from package.json
 
-ReactDOM.render(
-  <BrowserRouter basename={PUBLIC_URL}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
